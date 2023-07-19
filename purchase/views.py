58f2d4810f25f138f8ffe134fea11fbe0ee64090
Pavlo Kulka <126619088+KXIGIKI22@users.noreply.github.com>
2023-07-19 23:38:57 +0200

    Delete purchase directory

--- purchase/views.py
+++
@@ -1,6 +0,0 @@
-from django.http import JsonResponse
-from .models import Purchase
-
-def purchases(request):
-    purchases = Purchase.objects.all().values()
-    return JsonResponse(list(purchases), safe=False)
