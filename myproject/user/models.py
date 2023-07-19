b7739f04c02c39b107559018901c24e8cb094107
pavlo <pavlo.kulka@gmail.com>
2023-07-20 00:10:19 +0200

    Homework 37

--- myproject/user/models.py
+++ myproject/user/models.py
@@ -3,8 +3,7 @@ from django.db import models
 class User(models.Model):
     username = models.CharField(max_length=50)
     email = models.EmailField(unique=True)
-    title = models.CharField(max_length=100)
-    author = models.CharField(max_length=100)
+    # Add other fields as needed
 
     def __str__(self):
         return self.username
