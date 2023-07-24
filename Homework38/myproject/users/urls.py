8a8b7f7b32ef78ee0c008bbea135d677b190354c
pavlo <pavlo.kulka@gmail.com>
2023-07-24 22:10:01 +0200

    Homework 38

--- Homework38/myproject/users/urls.py
+++ Homework38/myproject/users/urls.py
@@ -1,6 +1,6 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('users/', include('users.urls')),
+    path('', views.user_list, name='user_list'),
+]
