60137417406e3c606ca5e80221cbb52c07359410
pavlo <pavlo.kulka@gmail.com>
2023-07-25 20:27:23 +0200

    Homework 39

--- Homework38/myproject/myproject/urls.py
+++ Homework38/myproject/myproject/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from .views import UserListView, UserDetailView, CreateUserView
 
 urlpatterns = [
+    path('users/', UserListView.as_view(), name='user_list'),
+    path('users/<int:pk>/', UserDetailView.as_view(), name='user_detail'),
+    path('users/create/', CreateUserView.as_view(), name='create_user'),
     path('admin/', admin.site.urls),
     path('users/', include('user.urls')),
     path('books/', include('book.urls')),
--- Homework38/myproject/users/views.py
+++ Homework38/myproject/users/views.py
@@ -1,7 +1,20 @@
-from django.http import JsonResponse
+from django.views.generic import ListView, DetailView
 from .models import User
+from django.views.generic.edit import CreateView
+from .forms import UserForm
 
-def user_list(request):
-    users = User.objects.all()
-    data = list(users.values())
-    return JsonResponse(data, safe=False)
+
+class UserListView(ListView):
+    model = User
+    template_name = 'user_list.html'
+
+class UserDetailView(DetailView):
+    model = User
+    template_name = 'user_detail.html'
+    context_object_name = 'user'
+
+class CreateUserView(CreateView):
+    model = User
+    form_class = UserForm
+    template_name = 'create_user.html'
+    success_url = '/users/'
